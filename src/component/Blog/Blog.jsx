620b2f709ff8e872654f12646db87a8d84d97543
contactmizan <contactmizan1206@gmail.com>
2024-03-16 03:09:13 +0600

    added single post

--- src/component/Blog/Blog.jsx
+++ src/component/Blog/Blog.jsx
@@ -1,10 +1,29 @@
 import PropTypes from 'prop-types';
 
 const Blog = ({ blog }) => {
-    console.log(blog);
+    //4. destructure
+    const { title, cover, author, posted_date, author_img, reading_time, hashtags } = blog;
     return (
         <div>
-
+            <img src={cover} alt={`Cover picture of the title ${title}`} />
+            <div className='flex justify-between'>
+                <div className='flex gap-6'>
+                    <img className='w-14 h-14 rounded-full' src={author_img} alt="" />
+                    <div>
+                        <h3 className='text-2xl'>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div>
+                    <span>{reading_time} min read</span>
+                </div>
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash, idx) => <span key={idx}> <a>#{hash}</a> </span>)
+                }
+            </p>
         </div>
     );
 }
