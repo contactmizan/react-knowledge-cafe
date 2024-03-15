e891b51ba02aa44512bd68201e32132b94b84893
contactmizan <contactmizan1206@gmail.com>
2024-03-15 16:40:59 +0600

    create header and blogs component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import Header from './component/Header/Header'
 import Blogs from './component/Blogs/Blogs'
+import Bookmarks from './component/Bookmarks/Bookmarks'
 
 function App() {
 
   return (
     <>
       <Header></Header>
-      <Blogs></Blogs>
+      {/* 3.jehutu 2 ta flex korte hobe tai div er moddhe neya hoiche */}
+      <div className='md:flex'>
+        <Blogs></Blogs>
+        <Bookmarks></Bookmarks>
+      </div>
 
     </>
   )
